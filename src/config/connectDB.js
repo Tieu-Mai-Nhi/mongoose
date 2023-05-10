f1818f7b3c47b59dfbcdc0ef277754f936a94e9e
Mai Duc Thang <thangmdfx12498@funix.edu.vn>
2023-05-10 15:56:43 +0700

    CRUD

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -3,29 +3,55 @@ mongoose.connect("mongodb://127.0.0.1/Nodemy");
 
 const Schema = mongoose.Schema;
 
-const account = new Schema({
-  username: String,
-  password: Number,
-  age: Number,
-});
+const account = new Schema(
+  {
+    username: String,
+    password: Number,
+    age: Number,
+  },
+  { collection: "Account" }
+  //   chọn đúng collection, ko auto thêm s
+);
 
 const AccountModel = mongoose.model("account", account);
 
-AccountModel.find({ username: "okok" })
-  .then(function (data) {
-    console.log("data: ", data);
-  })
-  .catch(function (e) {
-    console.log("error: ", e);
-  });
+// AccountModel.find({ username: "okok" })
+//   .then(function (data) {
+//     console.log("data: ", data);
+//   })
+//   .catch(function (e) {
+//     console.log("error: ", e);
+//   });
 
-AccountModel.create({
-  username: "hung",
-  password: 88888,
-})
-  .then(function (data) {
-    console.log("data: ", data);
-  })
-  .catch(function (e) {
-    console.log("error: ", e);
-  });
+// AccountModel.create({
+//   username: "hung",
+//   password: 88888,
+// })
+//   .then(function (data) {
+//     console.log("data: ", data);
+//   })
+//   .catch(function (e) {
+//     console.log("error: ", e);
+//   });
+
+AccountModel.updateOne(
+  // ĐIỀU KIỆN
+  {
+    username: "hung",
+  },
+  //CÁI CẦN SỬA Ở DATA MỚI
+  {
+    password: 123456,
+  }
+)
+  .then((data) => console.log("data: ", data))
+  .catch((e) => console.log("error: ", e));
+
+AccountModel.deleteMany(
+  // ĐIỀU KIỆN
+  {
+    username: "thang",
+  }
+)
+  .then((data) => console.log("data: ", data))
+  .catch((e) => console.log("error: ", e));
